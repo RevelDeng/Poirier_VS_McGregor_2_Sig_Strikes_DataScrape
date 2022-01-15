fb425f5e1d64c8c0245f7e501b77295decfd468a
RevelDeng <84701226+RevelDeng@users.noreply.github.com>
2022-01-15 01:19:59 -0500

    edited out some comments

--- poiriervsmcgregor2datascrape.py
+++ poiriervsmcgregor2datascrape.py
@@ -1,4 +1,4 @@
-#scrape significant strikes by head, body, and leg and calculate accuracy (sig. str %) by round as well as total
+#scrape significant strikes by head, body, and leg and calculate accuracy (sig. str %)
 import sys
 sys.path.append('C:\\users\\revel\\anaconda3\\lib\\site-packages')
 
@@ -10,16 +10,9 @@ soup = BeautifulSoup(r.text, 'html.parser')
 
 #find the tables of the page
 tableresults = soup.find_all('table', attrs = {'style': 'width: 745px'})
-# ~ print (len(tableresults))
 
 #work with the sig strikes table only and get the data
 allsigstrikes = tableresults[-1].find_all('p', attrs = {'class': 'b-fight-details__table-text'})
-# ~ print (len(allsigstrikes))
-
-# ~ for i in allsigstrikes:
-	# ~ print (i.text)
-
-# ~ print (allsigstrikes[6:12])
 
 strikenumbers = []
 
@@ -29,8 +22,6 @@ for i in range(6, 12):
 	for j in allsigstrikes[i].text.split():
 		if j.isdigit():
 			strikenumbers.append(int(j))
-			
-# ~ print (strikenumbers)
 
 strikepercents = []
 count = 0
@@ -38,16 +29,9 @@ while count < len(strikenumbers):
 	strikepercents.append("{:.0%}".format(strikenumbers[count] / strikenumbers[count + 1]))
 	count += 2
 	
-# ~ print (strikepercents)
-
-# ~ count2 = 0
-# ~ while count2 < len(strikepercents):
-	# ~ print ("Dustin Poirier
 poiriernum = strikepercents[::2]
 mcgregornum = strikepercents[1::2]
 targetlist = ["head", "body", "leg"]
-
-# ~ print (poiriernum, mcgregornum)
 
 for i in range(len(poiriernum)):
 	print ("UFC 257: Poirier's significant", targetlist[i], "strike %:", poiriernum[i])
